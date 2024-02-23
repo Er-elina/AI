2109850703d9ad252c680d05a0b66ce7069571e8
elina shah <shahelina53@gmail.com>
2024-02-23 17:27:00 +0545

    maffd

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 <div> this is new component</div>
 <p> steps new Ai file </p>
+<p>git status </p>
+<p> git add .</p>
+<p>git status </p>
+<p> git commit -m "message_title" -m "message_description"</p>
+<p> git clone https://github.com/Er-elina/AI.git   </p>
+<p> git branch -M main</p>
+<p>git branch</p>
+<p>get push -u origin main</p>
